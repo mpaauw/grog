fccf0ca90611747a627af05f50e319b9a5ea0b80
mpaauw <4207462+mpaauw@users.noreply.github.com>
2022-09-27 13:42:53 -0700

    test(cache): add unit tests for cache service

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,11 @@ module.exports = {
   globalSetup: '<rootDir>/tests/globalSetup.js',
   testTimeout: 60000,
   coverageThreshold: {
-    global: { // TODO: set min coverage limits after poc finished
-      statements: 0,
-      branch: 0,
-      functions: 0,
-      lines: 0
+    global: {
+      statements: 90,
+      branch: 90,
+      functions: 90,
+      lines: 90
     }
   }
 };
